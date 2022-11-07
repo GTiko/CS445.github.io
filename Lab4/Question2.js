dd4ba826849358ad8dc5587da02071c78a40dd53
GTiko <gemechutiko@gmail.com>
2022-11-07 16:26:55 -0600

    lab4

---
+++ Lab4/Question2.js
@@ -0,0 +1,24 @@
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+  logger() {
+    console.log("user:" + this.name);
+  }
+}
+class DecoratedUser {
+  constructor(user, street, state) {
+    this.user = user;
+    this.name = user.name;
+    this.street = street;
+    this.state = state;
+  }
+  logger() {
+    console.log(
+      "decorated user: " + this.name + ", " + this.street + ", " + this.state
+    );
+  }
+}
+const user = new User("Kelly");
+const decorated = new DecoratedUser(user, "Broadway", "New York");
+decorated.logger();
